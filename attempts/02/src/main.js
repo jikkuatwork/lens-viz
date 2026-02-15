0b946489a55c7f0f737f4b8191dfa7603aa32cbb
Jikku Jose <jikku@storybrain.com>
2026-02-15 14:39:56 +0400

    Remove OpenAI version

--- attempts/02/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router.js'
-import './app.css'
-
-const app = createApp(App)
-app.use(router)
-
-router.isReady().then(() => app.mount('#app'))
